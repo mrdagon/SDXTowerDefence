bd5dd9909a13a25061fe79d4069b1dceb6fd8699
UEMURA <mr.dagonn@gmail.com>
2015-03-05 14:01:21 +0900

    Revert "reset"
    This reverts commit a4491daa3ca5dc4d07747ac67951da793f305f2d.

--- Artifact.h
+++ Artifact.h
@@ -17,38 +17,148 @@ namespace SDX_TD
 		開発済
 	};
 
-	enum class RecipeType
-	{
-		//強化
-		攻撃,
-		連射,
-		射程,
-		支援,
-		麻痺,//準固有特性
-		鈍足,//準固有特性
-		吹飛,//準固有特性
-		防壊,//準固有特性
-		範囲,//レア特性
-		回収,//レア特性
-		強化,//レア特性
-		特攻,//固有特性
-		逆境,//固有特性
-		消費,//固有特性
-		Hp,//レア特性
-		Mp,//レア特性
-		Sp,//レア特性
-		COUNT
-	};
-
-	class Recipe
-	{
-
-
-	};
-
-	/**魔導具.*/
 	class Artifact
 	{
-		EnumArray<int, RecipeType> 強化Lv;
+	public:
+		static Artifact 所持S[200];
+		static Artifact レシピS[100];
+		static EnumArray<double, ArtifactType> 基本出現率;
+
+		ArtifactType 種類;
+		RecipeState 状態;
+		int Lv;
+		bool is祝福;//副効果の有無
+		double 攻撃;
+		double 連射;
+		double 射程;
+		double 範囲;
+		double 支援;
+		double 麻痺;
+		double 鈍足;
+		double 吹飛;
+		double 防壊;
+		double 回収;//回収速度+と回収率+
+		double 強化;//強化速度+
+		double 逆境;
+		double 消費;
+		int Hp;
+		int Mp;
+		double Sp;
+
+		static void MakeRecipe(DevelopType 開発種)
+		{
+			//性能の決定
+			for (int a = 0; a < 100; ++a)
+			{
+				double 補正 = std::abs(a / 10 - 4.5) + std::abs(a%10 - 4.5);
+				//補正値は0～8
+				//中央から離れる程レベルに補正が入る？
+				レシピS[a].性能決定(開発種 , (int)補正 - 1);
+				レシピS[a].状態 = RecipeState::不明;
+			}
+
+			レシピS[44].状態 = RecipeState::識別;
+			レシピS[45].状態 = RecipeState::識別;
+			レシピS[54].状態 = RecipeState::識別;
+			レシピS[55].状態 = RecipeState::識別;
+		}
+
+		/**レシピの更新処理から呼び出す.*/
+		/** レア率等は暫定*/
+		void 性能決定(DevelopType 開発種, int レベル補正)
+		{
+			//レベルの決定
+			Lv = std::min( TDSystem::工房レベル + Rand::Get(レベル補正) , 9999 );
+			int レア度 = レア度決定(開発種);
+			種類決定(レア度);
+
+			switch (種類)
+			{
+				case ArtifactType::トルネルージュ:
+				case ArtifactType::ジャガーノート:
+				case ArtifactType::雪風＆空魂:
+				case ArtifactType::皇剣シルヴァウス:
+				case ArtifactType::カランコエ:
+				case ArtifactType::フォトンルーメン:
+				case ArtifactType::紅蜂:
+				case ArtifactType::クレメンス:
+				case ArtifactType::スヴァーズ:
+				case ArtifactType::プリティワンド:
+				case ArtifactType::チューベローズ:
+				case ArtifactType::フィソステギア:
+				case ArtifactType::炎の宝珠:
+				case ArtifactType::氷の宝珠:
+				case ArtifactType::風の宝珠:
+				case ArtifactType::樹の宝珠:
+				case ArtifactType::炎の結晶:
+				case ArtifactType::氷の結晶:
+				case ArtifactType::風の結晶:
+				case ArtifactType::樹の結晶:
+				case ArtifactType::炎の欠片:
+				case ArtifactType::氷の欠片:
+				case ArtifactType::風の欠片:
+				case ArtifactType::樹の欠片:
+			}
+		}
+
+		int レア度決定(DevelopType 開発種)
+		{
+			double ram = Rand::Get(1.0);
+			//種類の決定
+			switch (開発種)
+			{
+			case DevelopType::啓示の書:
+				is祝福 = Rand::Coin(0.3);
+				if (ram < 1.0 / 50) return 3;
+				else if (ram < 1.0 / 5) return 2;
+				else if (ram < 1.0 / 2) return 1;
+				else return 0;
+				break;
+			case DevelopType::悟りの書:
+				is祝福 = Rand::Coin(0.1);
+				if (ram < 1.0 / 200) return 3;
+				else if (ram < 1.0 / 20) return 2;
+				else if (ram < 1.0 / 5) return 1;
+				else return 0;
+				break;
+			case DevelopType::閃きの書:
+				is祝福 = Rand::Coin(0.05);
+				if (ram < 1.0 / 1000) return 3;
+				else if (ram < 1.0 / 100) return 2;
+				else if (ram < 1.0 / 10) return 1;
+				else return 0;
+			default:
+				break;
+			}
+			return 0;
+		}
+
+		void 種類決定(int レア度)
+		{
+			int no;
+
+			switch (レア度)
+			{
+			case 0:
+				no = Rand::Get(11);
+				種類 = ArtifactType(no);
+				break;
+			case 1:
+				no = Rand::Get(3);
+				種類 = ArtifactType((int)ArtifactType::炎の宝珠 + no);
+				break;
+			case 2:
+				no = Rand::Get(3);
+				種類 = ArtifactType((int)ArtifactType::炎の結晶 + no);
+				break;
+			case 3:
+				no = Rand::Get(3);
+				種類 = ArtifactType((int)ArtifactType::炎の欠片 + no);
+				break;
+			}
+		}
 	};
+
+	Artifact Artifact::所持S[200];
+	Artifact Artifact::レシピS[100];
 }
